3355c38bb3695bfee5fd3f7f2edeac94af1c620e
11213 <you@example.com>
2023-09-07 21:37:02 +0800

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting jpp") + ", " + name;
+    return messages.getString("greeting jpp") + ", " + name.toUpperCase();
   }
 
   public static void main(String[] args) {
